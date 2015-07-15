e93a365822774035e0300b2d2db37d28268c4523
James Milne <s4b3r6@gmail.com>
2015-07-15 01:32:21 +0000

    Decoding correctly for Python 3

--- test.py
+++ test.py
@@ -24,51 +24,51 @@ class MyAppTestCase(unittest.TestCase):
     def test_home_title(self):
         """Test that the homepage calls itself Doccu - Home"""
         rv = self.app.get('/')
-        self.assertIn('<title>Doccu - Home</title>'.encode('utf-8'), rv.data)
+        self.assertIn('<title>Doccu - Home</title>'.encode('utf-8'), rv.data.decode('utf-8'))
 
     def test_home_search(self):
         """Test that the homepage has a search function"""
         rv = self.app.get('/')
-        self.assertIn('<form action="/search/" method="POST">'.encode('utf-8'), rv.data)
+        self.assertIn('<form action="/search/" method="POST">'.encode('utf-8'), rv.data.decode('utf-8'))
 
     def test_home_categories(self):
         """Test that the homepage lists categories"""
         rv = self.app.get('/')
-        self.assertIn('<h1>Categories:</h1>'.encode('utf-8'), rv.data)
+        self.assertIn('<h1>Categories:</h1>'.encode('utf-8'), rv.data.decode('utf-8'))
 
     def test_search_page(self):
         """Test that the search page is accessible"""
         rv = self.app.get('/search/')
-        self.assertIn('<form action="/search/" method="POST">'.encode('utf-8'), rv.data)
+        self.assertIn('<form action="/search/" method="POST">'.encode('utf-8'), rv.data.decode('utf-8'))
 
     def test_new_document(self):
         """Test that you can create a new document"""
         rv = self.app.post('/document/new/new/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'223344997766551100'})
-        self.assertIn('Form submitted for'.encode('utf-8'), rv.data)
+        self.assertIn('Form submitted for'.encode('utf-8'), rv.data.decode('utf-8'))
 
     def test_search_submit(self):
         """Test that the search page is useable"""
         self.app.post('/document/new/new/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'223344997766551100'})
         rv = self.app.post('/search/',data={'category':'testing','title':'','author':''})
-        self.assertIn('<h3>Categories Found for TESTING:</h3>'.encode('utf-8'), rv.data)
+        self.assertIn('<h3>Categories Found for TESTING:</h3>'.encode('utf-8'), rv.data.decode('utf-8'))
 
     def test_read_document(self):
         """Test that you can read a document"""
         self.app.post('/document/new/new/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'223344997766551100'})
         rv = self.app.get('document/0.Automated_Test/')
-        self.assertIn('<h2>Document: Automated Test</h2>'.encode('utf-8'), rv.data)
+        self.assertIn('<h2>Document: Automated Test</h2>'.encode('utf-8'), rv.data.decode('utf-8'))
 
     def test_read_document_markdown(self):
         """Test that Markdown turns into HTML correctly"""
         self.app.post('/document/new/new/', data = {'title':'Automated Markdown Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'# Title\n\n##Subtitle\n\n###Tagline\n\n* Bullet point\n\n*Italic*\n\n**Bold**\n\n***Both italic and bold***','identifier':'223344997766551100'})
         rv = self.app.get('/document/0.Automated_Markdown_Test/')
-        self.assertIn('<h3>Document Proper:</h3>\n<h1>Title</h1>\n<h2>Subtitle</h2>\n<h3>Tagline</h3>\n<ul>\n<li>Bullet point</li>\n</ul>\n<p><em>Italic</em></p>\n<p><strong>Bold</strong></p>\n<p><strong><em>Both italic and bold</em></strong></p>', rv.data)
+        self.assertIn('<h3>Document Proper:</h3>\n<h1>Title</h1>\n<h2>Subtitle</h2>\n<h3>Tagline</h3>\n<ul>\n<li>Bullet point</li>\n</ul>\n<p><em>Italic</em></p>\n<p><strong>Bold</strong></p>\n<p><strong><em>Both italic and bold</em></strong></p>', rv.data.decode('utf-8'))
 
     def test_approve_document(self):
         """Test that you can approve a document"""
         self.app.post('/document/new/new/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'223344997766551100'})
         rv = self.app.post('document/0.Automated_Test/approve/', data={'version':'0','date':'2020-08-08','date-renew':'2021-08-08','identifier':'223344997766551100'})
-        self.assertIn('Form submitted for', rv.data)
+        self.assertIn('Form submitted for', rv.data.decode('utf-8'))
 
     def test_expire_check_document(self):
         """Test that documents expire correctly"""
@@ -102,7 +102,7 @@ class MyAppTestCase(unittest.TestCase):
     def test_view_category(self):
         """Test that Categories display their contents correctly"""
         rv = self.app.get('category/testing/')
-        self.assertIn('<li>', rv.data)
+        self.assertIn('<li>', rv.data.decode('utf-8'))
 
     def test_access_denied(self):
         """Test that the access denied page is accessible"""
@@ -113,19 +113,19 @@ class MyAppTestCase(unittest.TestCase):
         """Test that editing a page without the correct ID fails"""
         self.app.post('/document/new/new/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'223344997766551100'})
         rv = self.app.post('/document/0.Automated_Test/edit/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'0000'})
-        self.assertIn('accessdenied', rv.data)
+        self.assertIn('accessdenied', rv.data.decode('utf-8'))
 
     def test_access_denied_approve(self):
         """Test that approving a page without the correct ID fails"""
         self.app.post('/document/new/new/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'223344997766551100'})
         rv = self.app.post('/document/0.Automated_Test/approve/', data={'version':'0','date':'2020-08-08','date-renew':'2021-08-08','identifier':'0000'})
-        self.assertIn('accessdenied', rv.data)
+        self.assertIn('accessdenied', rv.data.decode('utf-8'))
 
     def test_access_denied_create(self):
         """Test that creating a new document without the correct ID fails"""
         self.app.post('/document/new/new/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'223344997766551100'})
         rv = self.app.post('/document/new/new/', data = {'title':'Automated Test','category':'testing','descriptor':'This is an automated testing procedure.','preamble':'This is an automated testing procedure','document-proper':'This is n automated testing procedure','identifier':'0000'})
-        self.assertIn('accessdenied', rv.data)
+        self.assertIn('accessdenied', rv.data.decode('utf-8'))
 
     def test_log_exists(self):
         """Test that the log file is correctly created"""
